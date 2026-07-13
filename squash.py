3acdc33a50571e6c378e48b0330afb4d898afb12
Papeet2785 <carmeet2785@gmail.com>
2026-07-12 22:43:42 -0400

    Set variables for easier configuration at top.

--- squash.py
+++ squash.py
@@ -10,9 +10,22 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+#fun variables to edit :)
+screen_x = 1024
+screen_y = 768
+velocity_x_init = -1000
+velocity_y_init = 0
+ball_size = 30
+paddle_size_x = 18
+paddle_size_y = 80
+fps = 60
+paddle_movement = 1000
+paddle_bounce = 400
+
+
 #window init + decorations
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((screen_x, screen_y))
 pygame.display.set_caption("Squash")
 icon = pygame.image.load(resource_path('Assets/icon.ico'))
 pygame.display.set_icon(icon)
@@ -20,31 +33,32 @@ pygame.display.set_icon(icon)
 #Sprites
 #floor
 floor = pygame.image.load(resource_path('Assets/floor.png'))
-floor = pygame.transform.scale(floor, (600, 600))
+floor = pygame.transform.scale(floor, (screen_x, screen_y))
 
 #ball
 ball = pygame.image.load(resource_path(('Assets/ball.png'))).convert_alpha()
-ball = pygame.transform.scale(ball, (25,25))
-x_ball = 300
-y_ball =300
+ball = pygame.transform.scale(ball, (ball_size,ball_size))
+ball_x = screen_x/2
+ball_y =screen_y/2
 
 #human paddle
-paddle1 = pygame.image.load(resource_path(('Assets/paddle.png'))).convert_alpha()
-x_paddle1 = 5
-y_paddle1 = 280
+paddle = pygame.image.load(resource_path(('Assets/paddle.png'))).convert_alpha()
+paddle = pygame.transform.scale(paddle, (paddle_size_x, paddle_size_y))
+paddle_x = 5
+paddle_y = ((screen_y/2) - (paddle_size_y/2))
 
 #game over
 game_over = pygame.image.load(resource_path(('Assets/gameover.png'))).convert_alpha()
-game_over = pygame.transform.scale(game_over, (600, 600))
+game_over = pygame.transform.scale(game_over, (screen_x, screen_y))
 
 #restart message, cp pasted gemini
 font = pygame.font.Font(None, 48)
 res_surface = font.render("Press r to restart!", True, 'black')
-res_rect = res_surface.get_rect(center=(600 // 2, 600 // 4))
+res_rect = res_surface.get_rect(center=(screen_x // 2, screen_y // 4))
 
-#initializing velocity
-x_ball_vel = -1000
-y_ball_vel = 0
+#speed variables init
+ball_x_vel = velocity_x_init
+ball_y_vel = velocity_y_init
 
 #screen stuff I copy/pasted
 clock = pygame.time.Clock()
@@ -53,67 +67,67 @@ running = True
 while running:
     #spawning stuff
     screen.blit(floor, (0,0))
-    screen.blit(ball, (x_ball,y_ball))
-    if x_ball < 0:
+    screen.blit(ball, (ball_x,ball_y))
+    if ball_x < 0:
         screen.blit(game_over, (0,0))
         screen.blit(res_surface, res_rect)
     else:
-        screen.blit(paddle1, (x_paddle1,y_paddle1))
+        screen.blit(paddle, (paddle_x,paddle_y))
         
     #hitboxes
-    b_hbox = pygame.Rect(x_ball, y_ball, ball.get_width(), ball.get_height())
+    b_hbox = pygame.Rect(ball_x, ball_y, ball_size, ball_size)
     #elite paddle
-    p1_hbox_top = pygame.Rect(x_paddle1, y_paddle1, paddle1.get_width(), 15)
-    p1_hbox_mid = pygame.Rect(x_paddle1, y_paddle1+15, paddle1.get_width(), paddle1.get_height()-30)
-    p1_hbox_bot = pygame.Rect(x_paddle1, y_paddle1 + paddle1.get_height() -15, paddle1.get_width(), 15)
+    p1_hbox_top = pygame.Rect(paddle_x, paddle_y, paddle_size_x, paddle_size_y/3)
+    p1_hbox_mid = pygame.Rect(paddle_x, paddle_y + paddle_size_y/3, paddle_size_x, paddle_size_y/3)
+    p1_hbox_bot = pygame.Rect(paddle_x, paddle_y + paddle_size_y * 2/3, paddle_size_x, paddle_size_y/3)
 
     #ball vx
     #paddle collisions
     if b_hbox.colliderect(p1_hbox_mid) or b_hbox.colliderect(p1_hbox_top) or b_hbox.colliderect(p1_hbox_bot):
-        x_ball_vel *= -1
+        ball_x_vel *= -1
     #wall collisions
-    if x_ball > 600 - ball.get_width():
-        x_ball = 600 - ball.get_width()
-        x_ball_vel *= -1
-    x_ball += x_ball_vel * delta_time
+    if ball_x > screen_x - ball_size:
+        ball_x = screen_x - ball_size
+        ball_x_vel *= -1
+    ball_x += ball_x_vel * delta_time
 
     #ball vy
     #paddle collisions
     if b_hbox.colliderect(p1_hbox_top):
-        y_ball_vel -= 200
+        ball_y_vel -= paddle_bounce
     if b_hbox.colliderect(p1_hbox_bot):
-        y_ball_vel += 200 
+        ball_y_vel += paddle_bounce
     #wall collisions
-    if y_ball < 0:
-        y_ball = 0
-        y_ball_vel *= -1
-    if y_ball > 600 - ball.get_height():
-        y_ball = 600 - ball.get_height()
-        y_ball_vel *= -1  
-    y_ball += y_ball_vel * delta_time
+    if ball_y < 0:
+        ball_y = 0
+        ball_y_vel *= -1
+    if ball_y > screen_y - ball_size:
+        ball_y = screen_y - ball_size
+        ball_y_vel *= -1  
+    ball_y += ball_y_vel * delta_time
 
     #keys
     keys = pygame.key.get_pressed()
     #up
-    if (keys[pygame.K_w] or keys[pygame.K_UP]) and y_paddle1>10:
-        y_paddle1 -= 800*delta_time
+    if (keys[pygame.K_w] or keys[pygame.K_UP]) and paddle_y>10:
+        paddle_y -= paddle_movement*delta_time
     #down
-    if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and y_paddle1<520:
-        y_paddle1 += 800*delta_time
+    if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and paddle_y<(screen_y - paddle_size_y):
+        paddle_y += paddle_movement*delta_time
     #restart
     if (keys[pygame.K_r]):
-        y_paddle1 = 280
-        x_ball_vel = -1000
-        y_ball_vel = 0
-        x_ball = 300
-        y_ball =300
+        paddle_y = ((screen_y/2) - (paddle_size_y/2))
+        ball_x_vel = velocity_x_init
+        ball_y_vel = velocity_y_init
+        ball_x = screen_x/2
+        ball_y = screen_y/2
 
     #stuff I cp pasted from youtube for better window/fps stuff
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
-    delta_time = clock.tick(60) / 1000
+    delta_time = clock.tick(fps) / 1000
     delta_time = max(0.001, min(0.1, delta_time))
-    clock.tick(60)
+    clock.tick(fps)
 pygame.quit()
